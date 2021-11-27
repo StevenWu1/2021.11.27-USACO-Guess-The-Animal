cea38f325a547c7fa8cd72052ac8c8c24bee5ac9
Steven Wu <89153549+StevenWu1@users.noreply.github.com>
2021-11-26 22:48:28 -0600

    Update First thoughts.py

--- First thoughts.py
+++ First thoughts.py
@@ -1,34 +1,33 @@
 import sys
 
 sys.stdin = open("guess.in","r")
-
 sys.stdout = open("guess.out","w")
 
 def parse_input(lines):
- n = int(lines.pop(0))
- for line in lines:
-  animals, num, traits = line.strip(). split()
-  data(animals) = set(traits)
- return data
+    n = int(lines.pop(0))
+    for line in lines:
+        animals, num, traits = line.strip(). split()
+        data[animals] = set(traits)
+    return data
 
 def slowmethod(data):
- animals = data.keys()
- most_common = 0
- for a in animals:
-  for b in animals:
-   if a != b:
-    common = len(data[a].intersection(data[b]))
-    most_common = max(common, most_common)
- return most_common + 1
+    animals = data.keys()
+    most_common = 0
+    for a in animals:
+        for b in animals:
+            if a != b:
+                common = len(data[a].intersection(data[b]))
+                most_common = max(common, most_common)
+    return most_common + 1
  
- demo = ""
- 4
- bird 2 flies eatsworms
- cow 4 eatsgrass isawesome makesmilk goesmoo
- sheep 1 eatsgrass
- goat 2 makesmilk eatsgrass
- "".strip().split("\n")
+demo = """
+4
+bird 2 flies eatsworms
+cow 4 eatsgrass isawesome makesmilk goesmoo
+sheep 1 eatsgrass
+goat 2 makesmilk eatsgrass
+""".strip().split("\n")
+
+print(slowmethod(parse_input(sys.stdin.readlines())))
  
- print(slowmethod(parse_input(sys.stdin.readlines())))
- 
- close(sys.stdout)
+close(sys.stdout)
